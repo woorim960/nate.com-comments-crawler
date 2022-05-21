f760aa5fad4e7cbdcabb550ec464d30c2cb5bcfa
woorim960 <woorimprog@gmail.com>
2022-05-21 23:49:40 +0900

    Add: 코드 설명을 위한 주석 추가

--- app/app.py
+++ app/app.py
@@ -8,12 +8,15 @@ import pandas as pd # 액셀로 변환하기 위해 필요
 import datetime # 엑셀 파일명 끝에 '날짜'를 추가하기 위해 필요 (엑셀 파일 이름의 중복을 피할 수 있음)
 import src.init as Init
 
+# 입력 값 세팅
 SEARCHING_START_YEAR = int(input("탐색의 시작 연도를 입력해주세요(ex: 2020) > "))
 SEARCHING_LAST_YEAR = int(input("탐색의 마지막 연도를 입력해주세요(ex: 2021) > "))
 SEARCHING_START_MONTH = int(input("탐색의 시작 월을 입력해주세요(ex: 1) > "))
 SEARCHING_LAST_MONTH = int(input("탐색의 마지막 월을 입력해주세요(ex: 12) > "))
 COMMENT_SERACHING_START_PAGE = int(input("각 게시판의 댓글을 몇번 Page부터 검색하시겠습니까? (ex: 1) > "))
 COMMENT_SERACHING_LAST_PAGE = int(input("각 게시판의 댓글을 몇번 Page까지 검색하시겠습니까? (ex: 10) > "))
+
+# 게시판 댓글 많이 달린 순위 START_RANK 부터 LAST_RANK 까지 탐색
 SEARCHING_START_RANK = 1 # 랭크 1위부터 검색
 SEARCHING_LAST_RANK = 30 # 30위 까지가 최대임
 
@@ -22,18 +25,23 @@ browser = Init.get_chrome_driver()
 wait  = WebDriverWait(browser, 10) # 로딩 안됐을 때 10초까지 기다리게 해주는 도구 가져오기
 print()
 
-# 데이터 수집 시작
+# 탐색할 URL 불러오기
 urls = Init.getUrls([SEARCHING_START_YEAR, SEARCHING_LAST_YEAR, SEARCHING_START_MONTH, SEARCHING_LAST_MONTH])
-comments = []
+comments = [] # 최종 수집될 댓글이 저장된다.
+
+# 모든 URL들을 순회하며, 댓글을 수집한다.
 for url in urls:
   date = url.split("=")[-1]
-  year = date[:4]
-  month = date[4:6]
-  day = date[6:]
+  year = date[:4] # 연도
+  month = date[4:6] # 월
+  day = date[6:] # 일
   print(f"\n*************************  {year}년 {month}월 {day}일 주차의 기사를 탐색합니다  *************************")
-  browser.get(url)
+  browser.get(url) # 해당 URL로 이동한다.
+
+  # 이동한 URL(사이트)에서 게시판 랭킹 순위 START_LANK 부터 LAST_RANK까지 탐색한다.
   for i in range(SEARCHING_START_RANK, SEARCHING_LAST_RANK + 1):
     try:
+      # 아래의 CSS 태그에 해당하는 기사를 클릭하여 사이트를 이동한다.
       if i <= 5:
         # rank{1} 이상 {5} 이하는 형제 노드 중 'mlt01' 클래스 태그의 자식 중 'tit' 클래스 태그의 text가 기사 제목임
         title = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, f'.rank{i} + .mlt01 .tit')))
@@ -41,6 +49,7 @@ for url in urls:
         # rank{6} 이상은  형제 노드 중 'a' 태그의 text가 기사 제목임.
         title = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, f'.rank{i} + a')))
     except:
+      # except는 에러가 발생되면 실행된다.
       print(f"\nError: {year}.{month}.{day} 주차에는 {i}번 째 랭킹의 게시글이 존재하지 않습니다.\n에러가 무시되고 다음 주차를 탐색합니다.")
       # 브라우저 뒤로가기
       Init.back(browser, year, month, day, i)
@@ -58,6 +67,7 @@ for url in urls:
               wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, f'#articleView')))
               artc_sq = browser.current_url.split("?")[0].split("/")[-1]
             except:
+              # except는 에러가 발생되면 실행된다.
               print("\nError: '코로나' 관 련 기사 제목 클릭 중 에러가 발생했습니다.\n에러가 무시되고 다음 게시글을 탐색합니다.")
               # 브라우저 뒤로가기
               Init.back(browser, year, month, day, i)
@@ -69,6 +79,7 @@ for url in urls:
                 requestUrl = f"https://comm.news.nate.com/Comment/ArticleComment/List?artc_sq={artc_sq}&order=&cmtr_fl=0&prebest=0&clean_idx=&user_nm=&fold=&mid=n1006&domain=&argList=0&best=1&return_sq=&connectAuth=N&page={commentPage}#comment"
                 print(f"\n{commentPage} 페이지의 댓글을 탐색합니다.")
               except:
+                # except는 에러가 발생되면 실행된다.
                 print("\nError: 댓글 자료 요청 중 에러가 발생했습니다.\n에러가 무시되고 다음 게시글을 탐색합니다.")
                 break
               html = requests.get(requestUrl).content
